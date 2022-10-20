9e6400a30fe75075a17e2fd6cb15e780a86a4641
Andrea Castro Manzano <andrea.castro@sofka.local>
2022-10-20 16:58:17 -0500

    UpperCase, LowerCase y TitleCase Pipes

--- src/app/ventas/pages/basicos/basicos.component.ts
+++ src/app/ventas/pages/basicos/basicos.component.ts
@@ -6,11 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styles: [
   ]
 })
-export class BasicosComponent implements OnInit {
+export class BasicosComponent {
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
+  nombreLower: string = 'lorena';
+  nombreUpper: string = 'LORENA';
+  nombreCompleto: string = 'loREnA casTrO'
 
 }
