960ed5f26de134742d2156457a177b424a2b0834
Andrea Castro Manzano <andrea.castro@sofka.local>
2022-10-21 11:25:31 -0500

    Se cambia el idioma por defecto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,16 @@
-import { NgModule } from '@angular/core';
+import { LOCALE_ID, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { AppRouterModule } from './app-router.module';
 import { SharedModule } from './shared/shared.module';
 import { VentasModule } from './ventas/ventas.module';
+
+//Cambiar el locale de la app
+import localeEsCO from '@angular/common/locales/es-CO';
+import { registerLocaleData } from '@angular/common'
+
+registerLocaleData(localeEsCO);
 
 @NgModule({
   declarations: [
@@ -16,7 +22,9 @@ import { VentasModule } from './ventas/ventas.module';
     SharedModule,
     VentasModule
   ],
-  providers: [],
+  providers: [
+    { provide: LOCALE_ID, useValue: 'es-CO' }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
